f273004fdce8089c37d4629d22d3a6653be6831e
Siddharth <139606136+Siddharth-Pandore@users.noreply.github.com>
2024-12-04 21:50:28 +0530

    react-type-animated

--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
 
 const Speech = () => {
   return (
     <div className="bubbleContainer">
-      <div className="bubble">Text</div>
+      <div className="bubble">
+        <TypeAnimation
+          sequence={[
+            1000,
+            "Same substring at the start will only be typed out once, initially",
+            1000,
+            "Lorem ipsum dolor sit amet lerimat consectetur adipisicing.",
+            1000,
+          ]}
+          wrapper="span"
+          speed={40}
+          deletionSpeed={100}
+          repeat={Infinity}
+        />
+      </div>
       <img src="/man.png" alt="" />
     </div>
   );
