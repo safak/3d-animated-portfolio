c414a2bd0365140443b35c2a8fa745ba5e55a6b6
Armaan <the.armaan.mohamed@gmail.com>
2024-12-08 11:25:51 +0200

    Initial Setup

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./portfolio.css";
+
+const Portfolio = () => {
+  return (
+    <div className='portfolio'>Portfolio</div>
+  )
+}
+
+export default Portfolio
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./services.css";
+
+const Services = () => {
+  return (
+    <div className='services'>Services</div>
+  )
+}
+
+export default Services
